4d4eee078561c2d2c293d54f3759afda03ea2fed
Yuvraj Maurya <yuvraj95166@gmail.com>
2023-11-30 21:25:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+  const targetElement = document.getElementById('level');
+  let domLevel = 0;
+
+  if (targetElement) {
+    let parent = targetElement.parentElement;
+
+    while (parent) {
+      domLevel++;
+      parent = parent.parentElement;
+    }
+
+    // Adding 1 to include the root html element
+    domLevel++;
+
+    alert(`The level of the element is: ${domLevel}`);
+  } else {
+    alert('Element with id "level" not found.');
+  }
+});
